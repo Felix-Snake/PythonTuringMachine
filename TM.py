ee1d6a0caffdf4881bd513c15a30a0d15390055b
Felix <enotkomunizd0117@email.com>
2024-10-21 23:07:07 +0300

    Func 'Transformation'(All pricess of Turring Machine) is work

--- TM.py
+++ TM.py
@@ -4,14 +4,16 @@ class TM(): # Turing Machine
     inp = ""
     outp = ""
     steps = 0
-    current_status = ""
+    current_status = "q1"
     head_current_position = 0
+    break_count = 0
+    break_count_max = 3
 
     def __init__(self, operations:dict)->None:
-        self.operarions = operations
+        self.operations = operations
 
     def __call__(self, inp):
-        self.transformation()#Стандартная функция. Будет работать если простотобратиться к объекту через TM(input)
+        self.transformation(inp)#Стандартная функция. Будет работать если простотобратиться к объекту через TM(input)
     def __len__(self):
         return len(self.operations)
 
@@ -22,12 +24,23 @@ class TM(): # Turing Machine
         pass
 
     def transformation(self, inp:str):
-        self.steps = len(inp)
-        step_out = operations[self.inp[self.steps - self.head_current_position - 1] + current_status]
-        self.inp = inp.replace(inp[steps - head_current_position - 1], stepout[0])
+        self.inp = inp
+        print(self.inp)
+        while self.current_status != "q0":
+            self.steps = len(inp)
+            step_out = self.operations[self.inp[self.steps - self.head_current_position - 1] + self.current_status]
+        
 
-        self.head_current_position += 1
-        self.current_status = stepout[1:]
+            self._execute_operation(step_out)
+            if self.break_count >= self.break_count_max:
+                print("Stop process...")
+                break
+            print(f"↓ - {self.current_status}")
+            print(self.outp)
+            self.inp = self.outp
+        
+        #self.head_current_position += 1
+        #self.current_status = stepout[1:]
 
     def _execute_operation(self, operation:str)->None:
         move_code = {
@@ -39,16 +52,26 @@ class TM(): # Turing Machine
         
         self.head_current_position += move_code[operation[1].upper()]
 
+        if self.head_current_position >= len(self.inp): 
+                                             self.inp += "0"
+                                             self.break_count += 1
+        
+
         self.current_status = operation[2:]
 
-        
-    def _move_head_position():
-        pass
 
+        
     def _del_null_elements(inp:str)->str:
         return str(int(inp))
 
 
 
+if __name__ == '__main__':
+    operations = {
+        "0q1":"1Rq1",
+        "1q1":"0Rq0"
 
+        }
+    t = TM(operations)
+    t("00010")
 

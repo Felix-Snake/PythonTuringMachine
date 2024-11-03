eaccb175a93f2e94a5394a1256d27f78c68044ca
Felix <enotkomunizd0117@email.com>
2024-11-03 16:40:43 +0300

    Make full working model. Work transfomation() and _execute(). Project on 85% is done

--- TM.py
+++ TM.py
@@ -1,46 +1,50 @@
 
-class TM(): # Turing Machine
-    # По PEP8 здесь должно быть пояснение
+class TM():
+    """Turing Machine 
+        operations : The dict of strings with instructions for Turing Machine
+    """
+
     inp = ""
     outp = ""
     steps = 0
     current_status = "q1"
     head_current_position = 0
     break_count = 0
-    break_count_max = 3
+    break_count_max = 2
 
     def __init__(self, operations:dict)->None:
         self.operations = operations
 
-    def __call__(self, inp):
-        self.transformation(inp)#Стандартная функция. Будет работать если простотобратиться к объекту через TM(input)
+    def __call__(self, inp:str)->None:
+        """Standart_Function 
+            inp : string of 0's and 1's for processing by a Turing Machine
+        """
+        self.transformation(inp)#The standard function. It will work if you just access the object via TM(input)
+
     def __len__(self):
         return len(self.operations)
-
-    def __add__(self, TM2):#plus
-        pass
-
-    def __sub__(self, TM2): # minus
-        pass
 
     def transformation(self, inp:str):
         self.inp = inp
         print(self.inp)
         while self.current_status != "q0":
-            self.steps = len(inp)
-            step_out = self.operations[self.inp[self.steps - self.head_current_position - 1] + self.current_status]
+            self.steps = len(self.inp)
+            step_out = self.operations[self.inp[self.head_current_position] + self.current_status]
         
-
             self._execute_operation(step_out)
-            if self.break_count >= self.break_count_max:
+            if self.break_count > self.break_count_max:
                 print("Stop process...")
+                print("The Turing Machine never stop")
+                print(f"↓ - {self.current_status}")
+                print(self.outp)
+                self.inp = self.outp
+
                 break
+
             print(f"↓ - {self.current_status}")
             print(self.outp)
             self.inp = self.outp
         
-        #self.head_current_position += 1
-        #self.current_status = stepout[1:]
 
     def _execute_operation(self, operation:str)->None:
         move_code = {
@@ -52,9 +56,9 @@ class TM(): # Turing Machine
         
         self.head_current_position += move_code[operation[1].upper()]
 
-        if self.head_current_position >= len(self.inp): 
-                                             self.inp += "0"
-                                             self.break_count += 1
+        if self.head_current_position > (len(self.outp) - 1): 
+            self.outp += "0"
+            self.break_count += 1
         
 
         self.current_status = operation[2:]
@@ -69,9 +73,12 @@ class TM(): # Turing Machine
 if __name__ == '__main__':
     operations = {
         "0q1":"1Rq1",
-        "1q1":"0Rq0"
-
+        "1q1":"0Rq2",
+        "0q2":"0Rq2",
+        "1q2":"1Rq1",
+        "1q2":"1Rq0"
         }
+
     t = TM(operations)
-    t("00010")
+    t(input())
 
